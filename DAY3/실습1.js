77b0ebba57716965686ba74e8ba3e4f756ba7672
tjwls04 <y04y048@gmail.com>
2026-08-31 17:56:21 +0900

    feat: DAY3 실습 및 과제 내용 추가

---
+++ DAY3/실습1.js
@@ -0,0 +1,7 @@
+const movieList = document.querySelector("#movie-list");
+
+const card = document.createElement("div");
+card.textContent = "인셉션";
+card.classList.add("card");
+
+movieList.append(card);
